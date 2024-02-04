29111017e5955c83ba2245562e8bc2d432ad200f
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-02-04 12:50:21 +0300

    Implement auth hook

---
+++ frontend/src/hooks/useAuth.ts
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useUser } from "./useUser";
+import { useLocalStorage } from "./useLocalStorage";
+import { User } from "../models/User";
+
+export const useAuth = () => {
+  const { user, addUser, removeUser } = useUser();
+  const { getItem } = useLocalStorage();
+
+  //   TODO: Make sure useEffect works as expected
+  useEffect(() => {
+    const user = getItem("user");
+    if (user) {
+      addUser(JSON.parse(user));
+    }
+  }, []);
+
+  const login = (user: User) => {
+    addUser(user);
+  };
+
+  const logout = () => {
+    removeUser();
+  };
+
+  return { user, login, logout };
+};
