118c5c28eda21774e3f40ff579cf56ef1f649e20
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-02-05 22:48:54 +0300

    Wrap home page with auth outlet (protected)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,18 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import { useAuth } from "./hooks/useAuth";
 import { Home } from "./components/Home";
 import { Login } from "./components/Login";
 import { Register } from "./components/Register";
-import { AuthContext } from "./context/AuthContext";
+import AuthOutlet from "@auth-kit/react-router/AuthOutlet";
 
 export const App = () => {
-  const { user, setUser } = useAuth();
-
   return (
-    <>
-      <AuthContext.Provider value={{ user, setUser }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </AuthContext.Provider>
-    </>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route element={<AuthOutlet fallbackPath="/login" />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
   );
 };
