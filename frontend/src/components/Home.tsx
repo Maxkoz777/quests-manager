acb259ee280539d5aa43ab94f37af1255bce4412
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-02-05 22:49:20 +0300

    Clean up home page

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,23 +1,10 @@
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
-import { LogoutButton } from "./Logout";
-import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
 import { Base } from "./Base";
 
 export const Home = () => {
-  const { user } = useContext(AuthContext);
   return (
     <Base>
       <>
-        {user ? (
-          <LogoutButton />
-        ) : (
-          <Link to={{ pathname: "login" }}>
-            <Button variant="contained">Login</Button>
-          </Link>
-        )}
-        <h1>Hello there! {`${user ? user.name : "Stranger"}`}</h1>
+        <h1>Hello there! Stranger</h1>
       </>
     </Base>
   );
