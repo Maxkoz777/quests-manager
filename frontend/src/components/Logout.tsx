c71ec22d4923618eaa6f67c6bbe6a51d69b96e12
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-02-05 22:50:39 +0300

    Implement logout using auth kit

--- frontend/src/components/Logout.tsx
+++ frontend/src/components/Logout.tsx
@@ -1,7 +1,22 @@
-import { useAuth } from "../hooks/useAuth";
+import { Button } from "@mui/material";
+import useSignOut from "react-auth-kit/hooks/useSignOut";
+import { useNavigate } from "react-router-dom";
 
 export const LogoutButton = () => {
-  const { logout } = useAuth();
+  const signOut = useSignOut();
+  const navigate = useNavigate();
 
-  return <button onClick={logout}>Logout</button>;
+  const handleLogout = () => {
+    const signedOut = signOut();
+
+    if (signedOut) {
+      navigate("/login");
+    }
+  };
+
+  return (
+    <Button variant="contained" onClick={handleLogout}>
+      Logout
+    </Button>
+  );
 };
