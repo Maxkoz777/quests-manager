9fccf962a52a90289784481cdfc95de46a424bd1
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-02-08 23:36:01 +0300

    Display cards and a loading screen

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,26 +2,27 @@ import { useEffect, useState } from "react";
 import { Base } from "./Base";
 import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
 import axios, { AxiosError } from "axios";
-import { User } from "../models/User";
+import { API_URL } from "../utils/ApiUtils";
+import { Card, CardContent, Grid, Typography } from "@mui/material";
+import { TodoItems } from "../models/TodoItems";
 
 export const Home = () => {
   const [error, setError] = useState("");
-  const [user, setUser] = useState<User>();
+  const [todoItems, setTodoItems] = useState<TodoItems>();
+  const [loading, setLoading] = useState<boolean>(true);
   const authHeader = useAuthHeader();
 
   useEffect(() => {
     try {
       const fetchUser = async () => {
-        const { data } = await axios.get<User>(
-          "https://dummyjson.com/auth/me",
-          {
-            headers: {
-              Authorization: authHeader,
-            },
-          }
-        );
+        const { data } = await axios.get<TodoItems>(`${API_URL}/todos`, {
+          headers: {
+            Authorization: authHeader,
+          },
+        });
 
-        setUser({ ...data });
+        data && setTodoItems(data);
+        setLoading(false);
       };
 
       fetchUser();
@@ -36,12 +37,47 @@ export const Home = () => {
     }
   }, [authHeader]);
 
+  /**
+   * Truncate text such that all cards will have same max length of texts
+   */
+
   return (
     <Base>
-      <>
-        <h1>Hello there! {`${user ? user.username : "Stranger!"}`}</h1>
-        {error && <div>{error}</div>}
-      </>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <Grid
+          sx={{ height: "100%", display: "flex" }}
+          container
+          direction="row"
+          alignItems="stretch"
+          spacing={2}
+        >
+          {todoItems &&
+            todoItems.todos.map((item, key) => (
+              <Grid flex={"1"} item xs={12} sm={6} md={3} key={key}>
+                <Card key={key}>
+                  <CardContent>
+                    <Typography variant="h6" component="div">
+                      {item.todo}
+                    </Typography>
+                    <Typography variant="h6" component="div">
+                      Status:{" "}
+                      <span
+                        style={{
+                          color: `${item.completed ? "green" : "red"}`,
+                        }}
+                      >
+                        {item.completed.toString()}
+                      </span>
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          {error && <div>{error}</div>}
+        </Grid>
+      )}
     </Base>
   );
 };
