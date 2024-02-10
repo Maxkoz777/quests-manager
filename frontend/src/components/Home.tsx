681c699d4e9404e8a2b7581c1a380fe75b129da2
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-02-10 22:01:33 +0300

    Add address to the cards

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -5,12 +5,40 @@ import axios, { AxiosError } from "axios";
 import { API_URL } from "../utils/ApiUtils";
 import { Card, CardContent, Grid, Typography } from "@mui/material";
 import { TodoItems } from "../models/TodoItems";
+import { Address } from "../models/Addresses";
+import { Link } from "react-router-dom";
+import { TruncatedText } from "./TruncatedText";
 
 export const Home = () => {
   const [error, setError] = useState("");
   const [todoItems, setTodoItems] = useState<TodoItems>();
+  const [address, setAddress] = useState<Address[]>();
   const [loading, setLoading] = useState<boolean>(true);
   const authHeader = useAuthHeader();
+
+  useEffect(() => {
+    try {
+      const fetchAddress = async () => {
+        const { data } = await axios.get<Address[]>(
+          "https://random-data-api.com/api/v2/addresses?size=30"
+        );
+
+        data && setAddress(data);
+
+        setLoading(false);
+      };
+
+      fetchAddress();
+    } catch (err) {
+      if (err && err instanceof AxiosError) {
+        setError(err.response?.data.message);
+      } else if (err && err instanceof Error) {
+        setError(err.message);
+      }
+
+      console.error("Error: ", err);
+    }
+  }, []);
 
   useEffect(() => {
     try {
@@ -46,34 +74,31 @@ export const Home = () => {
           {todoItems &&
             todoItems.todos.map((item, key) => (
               <Grid item xs={12} sm={6} md={3} key={key}>
-                <Card key={key}>
-                  <CardContent>
-                    <Typography
-                      variant="h6"
-                      component="div"
-                      sx={{
-                        whiteSpace: "nowrap",
-                        overflow: "hidden",
-                        textOverflow: "ellipsis",
-                      }}
-                    >
-                      {item.todo}
-                    </Typography>
-                    <Typography variant="h6" component="div">
-                      Status:{" "}
-                      <span
-                        style={{
-                          color: `${item.completed ? "green" : "red"}`,
-                        }}
-                      >
-                        {item.completed.toString()}
-                      </span>
-                    </Typography>
-                    <Typography variant="h6" component="div">
-                      Address:
-                    </Typography>
-                  </CardContent>
-                </Card>
+                <Link
+                  to={{ pathname: "/task-detail" }}
+                  style={{ textDecoration: "none" }}
+                >
+                  <Card key={key}>
+                    <CardContent>
+                      <TruncatedText text={item.todo} />
+                      <Typography variant="h6" component="div">
+                        Status:{" "}
+                        <span
+                          style={{
+                            color: `${item.completed ? "green" : "red"}`,
+                          }}
+                        >
+                          {item.completed.toString()}
+                        </span>
+                      </Typography>
+                      <TruncatedText
+                        text={`Address: ${
+                          address && address[key].full_address
+                        }`}
+                      />
+                    </CardContent>
+                  </Card>
+                </Link>
               </Grid>
             ))}
           {error && <div>{error}</div>}
