33a834cd834893d149a57e165f4004933d86c500
Maxim Kozhinov <Maxkozhinov777@gmail.com>
2024-02-18 02:17:25 +0300

    DemoController added for testing purposes

---
+++ backend/prototype/src/main/java/com/quests/backend/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.quests.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello from Demo controller";
+    }
+
+}
