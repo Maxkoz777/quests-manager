ac689cacc544b375ab3df7d36db1c1e4aa42d0dd
Maxim Kozhinov <Maxkozhinov777@gmail.com>
2024-02-19 18:49:07 +0300

    added base CRUD operation & endpoints exposed

--- backend/prototype/src/main/java/com/quests/backend/service/OrderService.java
+++ backend/prototype/src/main/java/com/quests/backend/service/OrderService.java
@@ -3,6 +3,7 @@ package com.quests.backend.service;
 import com.quests.backend.model.entity.Order;
 import com.quests.backend.repository.OrderRepository;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,14 @@ public class OrderService {
         orderRepository.save(order);
         log.info("Order with id={} created", order.getId());
         return order.getId();
+    }
+
+    public Optional<Order> getOrderById(Long id) {
+        return orderRepository.findById(id);
+    }
+
+    public void deleteOrderById(Long id) {
+        orderRepository.deleteById(id);
     }
 
 }
