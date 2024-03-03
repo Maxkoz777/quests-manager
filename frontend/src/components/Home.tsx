08802e92031293ddc9bfd02fa32005d46c2d7c14
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-03-03 14:53:49 +0300

    Load orders and map on home page

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,22 +1,86 @@
+import { useEffect, useState } from "react";
 import { Base } from "./Base";
-import { Grid, Paper } from "@mui/material";
+import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
+import axios, { AxiosError } from "axios";
+import { API_URL } from "../utils/ApiUtils";
+import { Card, CardContent, Grid, Paper, Stack } from "@mui/material";
+import { Order } from "../models/Orders";
+import { Link } from "react-router-dom";
+import { TruncatedText } from "./TruncatedText";
+import { MapView } from "./MapView";
 
 export const Home = () => {
+  const [error, setError] = useState("");
+  const [orders, setOrders] = useState<Order[]>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const authHeader = useAuthHeader();
+
+  useEffect(() => {
+    try {
+      const fetchUser = async () => {
+        const { data } = await axios.get<Order[]>(`${API_URL}/orders`, {
+          headers: {
+            Authorization: "Bearer please use auth header!! thanks )))",
+          },
+        });
+
+        data && setOrders(data);
+        setLoading(false);
+      };
+
+      fetchUser();
+    } catch (err) {
+      if (err && err instanceof AxiosError) {
+        setError(err.response?.data.message);
+      } else if (err && err instanceof Error) {
+        setError(err.message);
+      }
+
+      console.error("Error: ", err);
+    }
+  }, [authHeader]);
+
   return (
     <Base>
-      {
+      {loading ? (
+        "Loading..."
+      ) : (
         <Grid container spacing={2} sx={{ height: "calc(100vh - 6rem)" }}>
-          <Grid item xs={12} md={3}></Grid>
+          <Grid item xs={12} md={3}>
+            <Stack spacing={2}>
+              {orders &&
+                orders.map((item, key) => (
+                  <Grid item key={key}>
+                    <Link
+                      to={{ pathname: "/task-detail" }}
+                      style={{ textDecoration: "none" }}
+                    >
+                      <Card key={key}>
+                        <CardContent>
+                          <TruncatedText text={item.title} />
+                          <TruncatedText
+                            text={`Description: ${item.description}`}
+                          />
+                        </CardContent>
+                      </Card>
+                    </Link>
+                  </Grid>
+                ))}
+              {error && <div>{error}</div>}
+            </Stack>
+          </Grid>
           <Grid
             item
             xs={0}
             sx={{ display: { xs: "none", md: "block" } }}
             md={9}
           >
-            <Paper elevation={3} sx={{ height: "100%" }}></Paper>
+            <Paper elevation={3} sx={{ height: "100%" }}>
+              <MapView />
+            </Paper>
           </Grid>
         </Grid>
-      }
+      )}
     </Base>
   );
 };
