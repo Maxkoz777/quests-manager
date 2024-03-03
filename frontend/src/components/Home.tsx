3481d836419a08301691d98b9d72c415daa31074
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-03-03 13:42:40 +0300

    Clean up home and set grid layout

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,109 +1,22 @@
-import { useEffect, useState } from "react";
 import { Base } from "./Base";
-import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
-import axios, { AxiosError } from "axios";
-import { API_URL } from "../utils/ApiUtils";
-import { Card, CardContent, Grid, Typography } from "@mui/material";
-import { TodoItems } from "../models/TodoItems";
-import { Address } from "../models/Addresses";
-import { Link } from "react-router-dom";
-import { TruncatedText } from "./TruncatedText";
+import { Grid, Paper } from "@mui/material";
 
 export const Home = () => {
-  const [error, setError] = useState("");
-  const [todoItems, setTodoItems] = useState<TodoItems>();
-  const [address, setAddress] = useState<Address[]>();
-  const [loading, setLoading] = useState<boolean>(true);
-  const authHeader = useAuthHeader();
-
-  useEffect(() => {
-    try {
-      const fetchAddress = async () => {
-        const { data } = await axios.get<Address[]>(
-          "https://random-data-api.com/api/v2/addresses?size=30"
-        );
-
-        data && setAddress(data);
-
-        setLoading(false);
-      };
-
-      fetchAddress();
-    } catch (err) {
-      if (err && err instanceof AxiosError) {
-        setError(err.response?.data.message);
-      } else if (err && err instanceof Error) {
-        setError(err.message);
-      }
-
-      console.error("Error: ", err);
-    }
-  }, []);
-
-  useEffect(() => {
-    try {
-      const fetchUser = async () => {
-        const { data } = await axios.get<TodoItems>(`${API_URL}/todos`, {
-          headers: {
-            Authorization: authHeader,
-          },
-        });
-
-        data && setTodoItems(data);
-        setLoading(false);
-      };
-
-      fetchUser();
-    } catch (err) {
-      if (err && err instanceof AxiosError) {
-        setError(err.response?.data.message);
-      } else if (err && err instanceof Error) {
-        setError(err.message);
-      }
-
-      console.error("Error: ", err);
-    }
-  }, [authHeader]);
-
   return (
     <Base>
-      {loading ? (
-        "Loading..."
-      ) : (
-        <Grid container spacing={2}>
-          {todoItems &&
-            todoItems.todos.map((item, key) => (
-              <Grid item xs={12} sm={6} md={3} key={key}>
-                <Link
-                  to={{ pathname: "/task-detail" }}
-                  style={{ textDecoration: "none" }}
-                >
-                  <Card key={key}>
-                    <CardContent>
-                      <TruncatedText text={item.todo} />
-                      <Typography variant="h6" component="div">
-                        Status:{" "}
-                        <span
-                          style={{
-                            color: `${item.completed ? "green" : "red"}`,
-                          }}
-                        >
-                          {item.completed.toString()}
-                        </span>
-                      </Typography>
-                      <TruncatedText
-                        text={`Address: ${
-                          address && address[key].full_address
-                        }`}
-                      />
-                    </CardContent>
-                  </Card>
-                </Link>
-              </Grid>
-            ))}
-          {error && <div>{error}</div>}
+      {
+        <Grid container spacing={2} sx={{ height: "calc(100vh - 6rem)" }}>
+          <Grid item xs={12} md={3}></Grid>
+          <Grid
+            item
+            xs={0}
+            sx={{ display: { xs: "none", md: "block" } }}
+            md={9}
+          >
+            <Paper elevation={3} sx={{ height: "100%" }}></Paper>
+          </Grid>
         </Grid>
-      )}
+      }
     </Base>
   );
 };
