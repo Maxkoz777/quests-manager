d3bdf15220fb8c9644bbd3535045566f06dd6aef
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-03-06 20:55:39 +0300

    Add dev api url for json server

--- frontend/src/utils/ApiUtils.ts
+++ frontend/src/utils/ApiUtils.ts
@@ -1,3 +1,4 @@
 export const API_URL = import.meta.env.VITE_API_URL;
 export const AUTH_API_URL = import.meta.env.VITE_AUTH_API_URL;
 export const VITE_APP_TITLE = import.meta.env.VITE_APP_TITLE;
+export const VITE_JSON_SERVER_URL = import.meta.env.VITE_JSON_SERVER_URL;
