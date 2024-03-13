73d06c52bf2352b80321e3ba9d2ca230184d38a8
Maxim Kozhinov <Maxkozhinov777@gmail.com>
2024-03-13 17:21:16 +0300

    added fraud communication from order service

--- backend/order-service/src/main/java/com/quests/backend/service/OrderService.java
+++ backend/order-service/src/main/java/com/quests/backend/service/OrderService.java
@@ -1,12 +1,16 @@
 package com.quests.backend.service;
 
+import com.example.common.events.FraudCheckMessage;
+import com.example.common.events.FraudResultMessage;
 import com.example.common.events.NotificationMessage;
 import com.example.common.events.PaymentCreationMessage;
 import com.example.common.events.PaymentReservationMessage;
+import com.example.common.events.enums.OrderStatus;
 import com.quests.backend.model.entity.Order;
 import com.quests.backend.repository.OrderRepository;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +27,10 @@ public class OrderService {
     private KafkaTemplate<String, PaymentCreationMessage> kafkaTemplate;
 
     @Autowired
-    private KafkaTemplate<String, NotificationMessage> notificationMessageKafkaTemplate;
+    private KafkaTemplate<String, NotificationMessage> notificationKafkaTemplate;
+
+    @Autowired
+    private KafkaTemplate<String, FraudCheckMessage> fraudKafkaTemplate;
 
     private final OrderRepository orderRepository;
 
@@ -33,10 +40,16 @@ public class OrderService {
         return orders;
     }
 
-    public long createOrder(Order order) {
+    public void createOrder(Order order) {
+        log.info("Creating order \"{}\"", order.getTitle());
+        var traceId = UUID.randomUUID().toString();
+        order.setOrderStatus(OrderStatus.INITIATED);
         orderRepository.save(order);
         log.info("Order with id={} created", order.getId());
-        return order.getId();
+        log.info("Sending order for the fraud checking service");
+        var fraudCheckMessage = new FraudCheckMessage(traceId, order.getId(), order.getDescription());
+        fraudKafkaTemplate.send("fraud.check.initiate", fraudCheckMessage)
+            .whenComplete((result, error) -> log.info("Fraud check initiated for the orderId={}", order.getId()));
     }
 
     public Optional<Order> getOrderById(Long id) {
@@ -49,16 +62,27 @@ public class OrderService {
 
     public void initTaskExecution() {
         log.info("Payment process initiated");
-        var paymentMessage = new PaymentCreationMessage("jwt", "traceId", "orderId", 12.34);
+        var traceId = UUID.randomUUID().toString();
+        var paymentMessage = new PaymentCreationMessage("jwt", traceId, 1234, 12.34);
         kafkaTemplate.send("payment.initiate", paymentMessage);
+        log.info("Payment process initiating, returning result to client");
     }
 
     @KafkaListener(topics = "payment.reservation.updated")
     public void processPaymentNotification(PaymentReservationMessage message) {
         log.info("Received payment reservation message: {}", message);
-        var notification = new NotificationMessage(message.traceId(), "userId",
+        var notification = new NotificationMessage(message.traceId(), 1234,
                                                    "Payment validated, the task can be executed");
         log.info("Sending payment notification to user");
-        notificationMessageKafkaTemplate.send("user.notification.order.update", notification);
+        notificationKafkaTemplate.send("user.notification.order.update", notification)
+            .whenComplete((result, error) -> log.info("Notification successfully sent"));
+    }
+
+    @KafkaListener(topics = "fraud.check.completed")
+    public void processFraudValidationResponse(FraudResultMessage fraudResultMessage) {
+        log.info("Received fraud validation message: {}", fraudResultMessage);
+        var orderOptional = orderRepository.findById(fraudResultMessage.orderId());
+        orderOptional.ifPresent(order -> order.setOrderStatus(OrderStatus.CREATED));
+        log.info("Updating the status for order {}", fraudResultMessage.orderId());
     }
 }
