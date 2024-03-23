15ac6b626198c2d90f6638388c960e01adc586bf
Maxim Kozhinov <Maxkozhinov777@gmail.com>
2024-03-23 08:38:03 +0300

    Updated user service to utilise configuration properties

--- backend/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,9 +1,12 @@
 package com.example.userservice;
 
+import com.example.userservice.config.KeycloakConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(KeycloakConfigurationProperties.class)
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
