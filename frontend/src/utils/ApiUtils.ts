ed6ea5bb747175671de0bd341f77c9f1ee13e95a
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-03-24 14:16:34 +0300

    Clean up api utils

--- frontend/src/utils/ApiUtils.ts
+++ frontend/src/utils/ApiUtils.ts
@@ -1,7 +1,5 @@
 export const VITE_API_URL = import.meta.env.VITE_API_URL;
 
-export const AUTH_API_URL = import.meta.env.VITE_AUTH_API_URL;
-
 export const VITE_AUTH_LOGIN_URL = import.meta.env.VITE_AUTH_LOGIN_URL;
 export const VITE_AUTH_GRANT_TYPE = import.meta.env.VITE_AUTH_GRANT_TYPE;
 export const VITE_AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID;
@@ -10,7 +8,5 @@ export const VITE_AUTH_CLIENT_SECRET = import.meta.env.VITE_AUTH_CLIENT_SECRET;
 export const VITE_AUTH_REGISTER_URL = import.meta.env.VITE_AUTH_REGISTER_URL;
 
 export const VITE_APP_TITLE = import.meta.env.VITE_APP_TITLE;
-
-export const VITE_JSON_SERVER_URL = import.meta.env.VITE_JSON_SERVER_URL;
 
 export const VITE_YANDEX_API_KEY = import.meta.env.VITE_YANDEX_API_KEY;
