2cebc64002bae7e056fbce28d169603186a4b947
Maxim Kozhinov <Maxkozhinov777@gmail.com>
2024-04-13 08:08:33 +0300

    QM-90: Order updated with fields related to execution time limitation

--- backend/order-service/src/main/java/com/quests/backend/model/dto/OrderCreationRequest.java
+++ backend/order-service/src/main/java/com/quests/backend/model/dto/OrderCreationRequest.java
@@ -1,9 +1,14 @@
 package com.quests.backend.model.dto;
 
+import java.time.Instant;
+
 public record OrderCreationRequest (
     String title,
     String description,
     Double latitude,
     Double longitude,
+    Long executionDuration,
+    Instant executionPeriodStart,
+    Instant executionPeriodEnd,
     double cost
 ){}
