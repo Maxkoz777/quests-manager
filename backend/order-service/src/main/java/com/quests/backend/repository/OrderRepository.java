e700eb0e69d624e9dcfce3a83082fe014320dc84
Maxim Kozhinov <Maxkozhinov777@gmail.com>
2024-04-21 15:48:43 +0300

    QM-96: Import updated

--- backend/order-service/src/main/java/com/quests/backend/repository/OrderRepository.java
+++ backend/order-service/src/main/java/com/quests/backend/repository/OrderRepository.java
@@ -4,7 +4,6 @@ import com.example.common.events.enums.OrderStatus;
 import com.quests.backend.model.entity.Order;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findOrderByCreatorIdIsNotAndOrderStatusEquals(String creatorId, OrderStatus orderStatus);
