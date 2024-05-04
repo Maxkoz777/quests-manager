f3007d11a3d49b1045fb35cbe2388306e8a93814
Maxim Kozhinov <66336810+Maxkoz777@users.noreply.github.com>
2024-05-04 14:04:47 +0300

    QM-101: kafka running issue fixed (#105)

--- backend/order-service/src/main/java/com/quests/backend/controller/DemoController.java
+++ backend/order-service/src/main/java/com/quests/backend/controller/DemoController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/demo")
 @SecurityRequirement(name = "Keycloak")
+@CrossOrigin(origins = "*")
 @Tag(name = "Demo Controller", description = "Demo controller APIs")
 public class DemoController {
 
