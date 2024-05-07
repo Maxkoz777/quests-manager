a9a5097f3d9347392abd51e0e8909091d520775e
Maxim Kozhinov <Maxkozhinov777@gmail.com>
2024-05-07 22:03:14 +0300

    QM-107: OrderController.java updated with CORS

--- backend/order-service/src/main/java/com/quests/backend/controller/OrderController.java
+++ backend/order-service/src/main/java/com/quests/backend/controller/OrderController.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/orders")
